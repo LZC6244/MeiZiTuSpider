45920c674b8180759b24e59f5306ea32c270280d
LZC6244 <624486877@qq.com>
2018-10-11 11:27:30 +0800

    edit readme.md and codes

--- MeiZiTu_1/MeiZiTu/spiders/meizitu_spider.py
+++ MeiZiTu_1/MeiZiTu/spiders/meizitu_spider.py
@@ -5,7 +5,7 @@ import re
 
 
 class MeizituSpiderSpider(scrapy.Spider):
-    name = 'meizitu_spider'
+        name = 'meizitu_spider'
     allowed_domains = ['meizitu.com']
     start_urls = ['http://meizitu.com/']
     count = 1
@@ -20,17 +20,18 @@ class MeizituSpiderSpider(scrapy.Spider):
             '//div[@class="postContent"]//img/@src | //div[@class="pic"]//img/@src').extract()
         yield item
         print '结束爬取： 第 %s 页' % self.count
+        try:
+            max_page = response.xpath('//div[@id="wp_page_numbers"]/ul/li[last()]/a/@href').extract()[0]
+            max_page = int(self.pattern.search(max_page).group(0))
 
-        max_page = response.xpath('//div[@id="wp_page_numbers"]/ul/li[last()]/a/@href').extract()[0]
-        max_page = int(self.pattern.search(max_page).group(0))
-
-        # 如果count小于最大页数，则继续往下爬取
-        # 若不需要爬取完全部页数，可更改此处max_page
-        if self.count < max_page:
-            self.count += 1
-            print 'go to next page seccess.'
-            next_url = 'http://meizitu.com/a/more_' + str(self.count) + '.html'
-            if self.count < max_page - 1:
+            # 如果count小于最大页数，则继续往下爬取
+            # 若不需要爬取完全部页数，可更改此处max_page
+            if self.count < max_page:
+                self.count += 1
+                print 'go to next page success.'
+                next_url = 'http://meizitu.com/a/more_' + str(self.count) + '.html'
                 yield scrapy.Request(url=next_url, callback=self.parse)
-        else:
-            print 'end.'
+            else:
+                print 'end.'
+        except Exception as e:
+            print 'spider end.'
